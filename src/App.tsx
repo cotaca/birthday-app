2cc1672055b38f031a022b272557ebc0cb109ccf
Cedric <kaw.cedric@web.de>
2025-07-22 11:32:56 +0200

    run lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,91 @@
-import './App.css'
-import {useQuery} from "@apollo/client";
+import './App.css';
+import { useQuery } from '@apollo/client';
 
 import { gql } from './__generated__';
 
-const GET_BIRTHDAY_CHARACTERS = gql(/* GraphQL */`
-    query GetBirthdayCharacters($page: Int = 1) {
-        Page(page: $page) {
-            pageInfo {
-                hasNextPage
-            }
-            characters (
-                isBirthday: true
-            ) {
-                id
-                name {
-                    full
-                }
-                age
-                gender
-                image {
-                    large
-                }
-                media {
-                    nodes {
-                        title {
-                            romaji
-                            english
-                        }
-                    }
-                }
-            }
+const GET_BIRTHDAY_CHARACTERS = gql(/* GraphQL */ `
+  query GetBirthdayCharacters($page: Int = 1) {
+    Page(page: $page) {
+      pageInfo {
+        hasNextPage
+      }
+      characters(isBirthday: true) {
+        id
+        name {
+          full
         }
+        age
+        gender
+        image {
+          large
+        }
+        media {
+          nodes {
+            title {
+              romaji
+              english
+            }
+          }
+        }
+      }
     }
+  }
 `);
 
 function App() {
-    const { loading, error, data } = useQuery(GET_BIRTHDAY_CHARACTERS);
+  const { loading, error, data } = useQuery(GET_BIRTHDAY_CHARACTERS);
 
-    if (loading) return <p>Lade Daten...</p>;
-    if (error) return <p>Fehler: {error.message}</p>;
-    if (!data?.Page?.characters) return <p>Keine Charaktere gefunden.</p>;
+  if (loading) return <p>Lade Daten...</p>;
+  if (error) return <p>Fehler: {error.message}</p>;
+  if (!data?.Page?.characters) return <p>Keine Charaktere gefunden.</p>;
 
-    const characters = data.Page.characters;
-    const currentDate = new Date();
+  const characters = data.Page.characters;
+  const currentDate = new Date();
 
   return (
     <>
-        <h1>{currentDate.toLocaleDateString()}</h1>
-        <h2>Herzlichen Glückwunsch!</h2>
-        {characters.map((character) => {
-            if (!character?.media?.nodes) return <p>Ungültige Character-Daten erhalten.</p>;
+      <h1>{currentDate.toLocaleDateString()}</h1>
+      <h2>Herzlichen Glückwunsch!</h2>
+      {characters.map((character) => {
+        if (!character?.media?.nodes)
+          return <p key={character?.id}>Ungültige Character-Daten erhalten.</p>;
 
-            const origin = character.media.nodes.map((node) => node?.title?.romaji ?? "")
+        const origin = character.media.nodes.map(
+          (node) => node?.title?.romaji ?? ''
+        );
 
-            return (
-                <div style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    padding: 5
-                }}>
-                    <div>
-                        <h2>{character.name?.full ?? "Unbekannt"}</h2>
-                        <p><b>Alter:</b> {character.age ?? "Unbekannt"}</p>
-                        <p><b>Geschlecht:</b> {character.gender ?? "Unbekannt"}</p>
-                        <p><b>Herkunft:</b> {origin.join(", ")}</p>
-                    </div>
-                    <img
-                        src={character.image?.large ?? "invalid url"}
-                        alt={"Image of " + (character.name?.full ?? "invalid name")}
-                        width={160}
-                    />
-                </div>
-            )
-        })}
+        return (
+          <div
+            key={character.id}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              padding: 5,
+            }}
+          >
+            <div>
+              <h2>{character.name?.full ?? 'Unbekannt'}</h2>
+              <p>
+                <b>Alter:</b> {character.age ?? 'Unbekannt'}
+              </p>
+              <p>
+                <b>Geschlecht:</b> {character.gender ?? 'Unbekannt'}
+              </p>
+              <p>
+                <b>Herkunft:</b> {origin.join(', ')}
+              </p>
+            </div>
+            <img
+              src={character.image?.large ?? 'invalid url'}
+              alt={'Image of ' + (character.name?.full ?? 'invalid name')}
+              width={160}
+            />
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from '@apollo/client';
+import './index.css';
+import App from './App.tsx';
 
 const client = new ApolloClient({
-    link: new HttpLink({
-        uri: 'https://graphql.anilist.co',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-    }),
-    cache: new InMemoryCache()
+  link: new HttpLink({
+    uri: 'https://graphql.anilist.co',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  }),
+  cache: new InMemoryCache(),
 });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <ApolloProvider client={client}>
-          <App />
-      </ApolloProvider>
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);
